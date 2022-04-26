05c29c755cc37c2886fb72e2e0b1e46024488354
Harold Tudtud <haroldns@umich.edu>
2022-04-26 16:41:35 -0400

    Squashed commit of the following:
    commit 274f832d0503cfad4d6ee3e742c61f8e7a30be15
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:40:12 2022 -0400
    
        All methods being tested
    
    commit 145efe864854fefbc0af6e68d7ce07a94f2ab82f
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:39:37 2022 -0400
    
        testMixed implementation
    
    commit 520ede4ec5db71278580f05705d75857619ebe92
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:36:59 2022 -0400
    
        Implementation of testMixed
    
    commit d1552b9380d81176997e15e71967eeb125dcebdc
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:29:50 2022 -0400
    
        Working insertionSort with testPositive
    
    commit e7188a3e550dae221666c0d2371a53f7503c2e04
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:13:44 2022 -0400
    
        Syntax fixing
    
    commit 0f5074e86230e5deb0c24779ed45b0dcf9335dfa
    Author: Harold Tudtud <haroldns@umich.edu>
    Date:   Tue Apr 26 16:08:02 2022 -0400
    
        Update class to be proper java syntax

--- testInsertionSort.java
+++ testInsertionSort.java
@@ -2,25 +2,61 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import tests.Largest;
-
 public class testInsertionSort {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		testPositive();
+		testNegative();
+		testMixed();
 	}
 	
 	public void testPositive(){
+        int[] arr = new int[5];
+        arr[0] = 100;
+        arr[1] = 32;
+        arr[2] = 53;
+        arr[3] = 369;
+        arr[4] = 1;
+        
+        /** add tests to check for this unit test **/
+        sort mySortClass = new sort();
+        
+        mySortClass.insertionSort(arr, arr.length);
+        assertArrayEquals(new int[]{1,32,53,100,369}, arr);
 	    
 	}
 
 	public void testNegative(){
 	   
+        int[] arr = new int[7];
+        arr[0] = -8;
+        arr[1] = -1;
+        arr[2] = -6;
+        arr[3] = -7;
+        arr[4] = -2;
+        arr[5] = -10;
+        arr[6] = -11;
+        
+        /** add tests to check for this unit test **/
+        sort mySortClass = new sort();
+        
+        mySortClass.insertionSort(arr, arr.length);
+        assertArrayEquals(new int[]{-11, -10, -8, -7, -6, -2, -1}, arr);
 	}
     
 	public void testMixed(){
-       
+        int[] arr = new int[4];
+        arr[0] = 64;
+        arr[1] = -15;
+        arr[2] = 70;
+        arr[3] = 110;
+        
+        /** add tests to check for this unit test **/
+        sort mySortClass = new sort();
+        
+        mySortClass.insertionSort(arr, arr.length);
+        assertArrayEquals(new int[]{-15, 64, 70, 110}, arr);
     }
 
 }
